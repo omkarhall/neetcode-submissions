26f695dc5c1ce57179ed2cc3062197d15af241f0
omkarhall <122930372+omkarhall@users.noreply.github.com>
2026-08-27 13:42:23 -0500

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        memo = {}
+        def dfs(n):
+            if n == 0:
+                return 0
+            if n == 1 or n == 2:
+                return 1
+            if n in memo:
+                return memo[n]
+            memo[n] = dfs(n-1) + dfs(n-2) + dfs(n-3)
+            return memo[n]
+        return dfs(n)
